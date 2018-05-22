89c358d0919ea43fdaf9b5d7b848aabadc9405b3
vanncho <van_cho@abv.bg>
2018-05-22 21:34:36 +0300

    Delete unnecessary folder

--- GameStore - Angular + Spring Boot + MySQL/client/out-tsc/app/app/core/guards/index.js
+++
@@ -1,7 +0,0 @@
-import { AuthGuard } from './auth/auth.guard';
-import { AdminGuard } from './admin/admin.guard';
-export var guardsModules = [
-    AuthGuard,
-    AdminGuard
-];
-//# sourceMappingURL=index.js.map
